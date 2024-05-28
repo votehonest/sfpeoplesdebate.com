ec934772229669c2a4a9fdb9c8065de91c44d157
Honest Charley Bodkin <hcbodkin@gmail.com>
2024-05-27 18:29:36 -0700

    fix description

--- apps/sfpeoplesdebate/src/app/metadata.tsx
+++ apps/sfpeoplesdebate/src/app/metadata.tsx
@@ -4,5 +4,5 @@ export const metadata = {
       The San Francisco People’s Debate for Mayor 2024 is a series of weekly
       debates between the candidates for Mayor of San Francisco held
       in Mission Dolores Park on Saturdays from 1 PM - 3 PM.
-    `,
+    `.replace(/\s+/g, ' '),
 };
